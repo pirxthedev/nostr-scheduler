56dd06a96a075fcc9463cb0268bcbb9d635b2586
Pirx <82919676+pirxthedev@users.noreply.github.com>
2023-03-25 11:54:36 -0400

    folder reorganization

--- src/cloudflare/index.test.ts
+++ src/cloudflare/index.test.ts
@@ -6,7 +6,7 @@ describe("Worker", () => {
 	let worker: UnstableDevWorker;
 
 	beforeAll(async () => {
-		worker = await unstable_dev("src/index.ts", {
+		worker = await unstable_dev("src/cloudflare/index.ts", {
 			experimental: { disableExperimentalWarning: true },
 		});
 	});
