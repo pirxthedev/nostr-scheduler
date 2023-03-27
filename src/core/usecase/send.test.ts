0a2edac81ecd08f14759d3e7a8e0475afca6dcac
Pirx <82919676+pirxthedev@users.noreply.github.com>
2023-03-26 21:47:06 -0400

    add test coverage

--- src/core/usecase/send.test.ts
+++ src/core/usecase/send.test.ts
@@ -6,6 +6,13 @@ import { MockStorage } from '../mocks';
 
 const nostr = new MockNostr();
 
+const validScheduledNote = {
+    note: {
+        content: 'valid',
+        created_at: 0
+    }
+};
+
 
 describe('Use Case: Send Scheduled Notes', () => {
     it('should retrieve scheduled notes from storage', () => {
@@ -21,14 +28,20 @@ describe('Use Case: Send Scheduled Notes', () => {
         expect(spy).not.toHaveBeenCalled();
     });
     it('should send one note if it is scheduled', () => {
-        const storage = new MockStorage([{
-            note: {
-                content: 'valid',
-                created_at: 0
-            }
-        }]);
+        const storage = new MockStorage([validScheduledNote]);
         const spy = vi.spyOn(nostr, 'sendNote');
         sendScheduledNotes(storage, nostr);
         expect(spy).toHaveBeenCalledOnce();
+        expect(spy).toHaveBeenCalledWith(validScheduledNote.note);
+    });
+    it('should send multiple notes if they are scheduled', () => {
+        const storage = new MockStorage([
+            validScheduledNote,
+            validScheduledNote
+        ]);
+        const spy = vi.spyOn(nostr, 'sendNote');
+        sendScheduledNotes(storage, nostr);
+        expect(spy).toHaveBeenCalledTimes(2);
+        expect(spy).toHaveBeenCalledWith(validScheduledNote.note);
     });
 });
